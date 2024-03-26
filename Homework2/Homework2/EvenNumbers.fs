57790b7969461bdba8787332109a82079df3fdb6
kazbekovruslan <chartusss@yandex.ru>
2024-03-26 13:52:22 +0300

    add abs to other two functions

--- Homework2/Homework2/EvenNumbers.fs
+++ Homework2/Homework2/EvenNumbers.fs
@@ -1,12 +1,11 @@
 module Homework2.EvenNumbers
 
-let countEvenWithMap list = 
+let countEvenWithMap list =
     list |> List.map (fun number -> if abs number % 2 = 0 then 1 else 0) |> List.sum
 
 let countEvenWithFilter list =
-    list |> List.filter (fun number -> number % 2 = 0) |> List.length 
+    list |> List.filter (fun number -> abs number % 2 = 0) |> List.length
 
-let countEvenWithFold list = 
-    list |> List.fold (fun state number -> if number % 2 = 0 then state + 1 else state) 0
-
-printf $"{countEvenWithMap [1..5]}"
+let countEvenWithFold list =
+    list
+    |> List.fold (fun state number -> if abs number % 2 = 0 then state + 1 else state) 0
