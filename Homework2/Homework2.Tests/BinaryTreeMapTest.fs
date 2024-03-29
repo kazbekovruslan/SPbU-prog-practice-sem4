279c8d05d52aff9d9ef90f145881406368eb7f48
kazbekovruslan <chartusss@yandex.ru>
2024-03-29 16:58:52 +0300

    add types

--- Homework2/Homework2.Tests/BinaryTreeMapTest.fs
+++ Homework2/Homework2.Tests/BinaryTreeMapTest.fs
@@ -5,12 +5,12 @@ open FsUnit
 open Homework2.BinaryTreeMap
 
 let testCases () =
-    [ (fun x -> 0), Node(1, Node(2, Leaf, Leaf), Leaf), Node(0, Node(0, Leaf, Leaf), Leaf)
-      (fun x -> x + 1), Node(1, Leaf, Node(2, Leaf, Leaf)), Node(2, Leaf, Node(3, Leaf, Leaf))
-      (fun x -> x + 1), Leaf, Leaf ]
+    [ ((fun x -> 0): int -> int), Node(1, Node(2, Leaf, Leaf), Leaf), Node(0, Node(0, Leaf, Leaf), Leaf)
+      ((fun x -> x + 1): int -> int), Node(1, Leaf, Node(2, Leaf, Leaf)), Node(2, Leaf, Node(3, Leaf, Leaf))
+      ((fun x -> x + 1): int -> int), Leaf, Leaf ]
     |> List.map (fun (func, tree, expected) -> TestCaseData(func, tree, expected))
 
 
-// [<TestCaseSource(nameof (testCases))>]
-// let BinaryTreeMapTest func tree expected =
-//     binTreeMap tree func |> should equal expected
+[<TestCaseSource(nameof (testCases))>]
+let BinaryTreeMapTest (func: int -> int) tree expected =
+    binTreeMap tree func |> should equal expected
