e80abe52d01b520669282c191666ecee94c78144
kazbekovruslan <chartusss@yandex.ru>
2024-03-29 17:42:14 +0300

    fix test

--- Homework2/Homework2.Tests/PrimeNumbersTest.fs
+++ Homework2/Homework2.Tests/PrimeNumbersTest.fs
@@ -8,6 +8,10 @@ let testCases () =
     [ 1, [ 2 ]; 4, [ 2; 3; 5; 7 ]; 10, [ 2; 3; 5; 7; 11; 13; 17; 19; 23; 29 ] ]
     |> List.map (fun (index, expected) -> TestCaseData(index, expected))
 
+//I did not fully understand why, without another declaring this function,
+//the test fails with an error: the source in Seq.take is null
+//so far it's just a workaround
+let generatePrimeNumbers = Seq.initInfinite (fun x -> x + 2) |> Seq.filter isPrime
 
 [<TestCaseSource(nameof (testCases))>]
 let isPrimeTest index expected =
