629bad874d3a5d2c912ee75d675ba141760ae5ce
kazbekovruslan <chartusss@yandex.ru>
2024-03-31 13:33:31 +0300

    complete tests

--- Homework3/Homework3.Tests/UnitTest1.fs
+++ Homework3/Homework3.Tests/UnitTest1.fs
@@ -5,5 +5,20 @@ open FsUnit
 open Homework3.LambdaInterpreter
 
 let testCases () =
-    []
-    |> List.map (fun (expression, expected) -> TestCaseData(expression, expected))
+    [ Var "x", Var "x"
+      Application((LambdaAbstraction("x", Var "x"), Var "z")), Var "z"
+      LambdaAbstraction("x", Var "a"), LambdaAbstraction("x", Var "a")
+      Application(
+          LambdaAbstraction("x", Var "y"),
+          Application(
+              LambdaAbstraction("x", LambdaAbstraction("x", Var "x")),
+              LambdaAbstraction("x", Application(Var "x", Var "x"))
+          )
+      ),
+      Var "y" ]
+    |> List.map (fun (expression, result) -> TestCaseData(expression, result))
+
+
+[<TestCaseSource(nameof (testCases))>]
+let reduceTests expression expected =
+    reduce expression |> should equal expected
