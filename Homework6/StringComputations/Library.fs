2e8c9700d56cdcc4b0e64eb5a51a739289a2cf1d
kazbekovruslan <chartusss@yandex.ru>
2024-05-09 23:47:22 +0300

    add impl

---
+++ Homework6/StringComputations/Library.fs
@@ -0,0 +1,11 @@
+﻿module StringComputations
+
+type StringComputationsBuilder() =
+    member this.Bind(x: string, f: int -> Result<int, string>) =
+        match System.Int32.TryParse x with
+        | (true, x) -> f x
+        | (false, _) -> Error "Error"
+
+    member this.Return(x: int) = Ok x
+
+let calculate = StringComputationsBuilder()
