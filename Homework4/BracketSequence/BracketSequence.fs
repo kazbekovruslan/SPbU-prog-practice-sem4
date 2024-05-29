99f72644724ba7907d9d18ac71fe7804eb475c22
kazbekovruslan <chartusss@yandex.ru>
2024-05-30 01:25:43 +0300

    fix bracket sequence checker

--- Homework4/BracketSequence/BracketSequence.fs
+++ Homework4/BracketSequence/BracketSequence.fs
@@ -1,32 +1,42 @@
 ﻿module BracketSequence
 
+let braces = [ ('(', ')'); ('[', ']'); ('{', '}') ]
+
+// without another declaring braces in function I have nullReferenceException
+let isOpeningBracket x =
+    let braces = [ ('(', ')'); ('[', ']'); ('{', '}') ]
+    braces |> List.exists (fun (opening, _) -> opening = x)
+
+let isClosingBracket x =
+    let braces = [ ('(', ')'); ('[', ']'); ('{', '}') ]
+    braces |> List.exists (fun (_, closing) -> closing = x)
+
+let isBracePair x y =
+    let braces = [ ('(', ')'); ('[', ']'); ('{', '}') ]
+    List.contains (x, y) braces
+
 let isBracketSequenceCorrect (str: string) =
-    let bracketPairs = [ ('(', ')'); ('[', ']'); ('{', '}') ]
-    let stack = System.Collections.Generic.Stack<char>()
+    let rec helper (stack, seq) =
+        match seq with
+        | [] -> List.isEmpty stack
+        | first :: seqTail when isOpeningBracket first -> helper ((first :: stack), seqTail)
+        | first :: seqTail when isClosingBracket first ->
+            match stack with
+            | [] -> false
+            | top :: stackTail ->
+                if isBracePair top first then
+                    helper (stackTail, seqTail)
+                else
+                    false
+        | _ :: seqTail -> helper (stack, seqTail)
 
-    str
-    |> Seq.forall (fun c ->
-        match c with
-        | '('
-        | '['
-        | '{' ->
-            stack.Push(c)
-            true
-        | ')'
-        | ']'
-        | '}' ->
-            if stack.Count = 0 then
-                false
-            else
-                let expected =
-                    bracketPairs
-                    |> List.tryFind (fun pair -> fst pair = stack.Peek())
-                    |> Option.map snd
+    helper ([], Seq.toList str)
 
-                match expected with
-                | Some expectedChar when expectedChar = c ->
-                    stack.Pop()
-                    true
-                | _ -> false
-        | _ -> true)
-    && stack.Count = 0
+
+
+let str1 = "[a]"
+
+let isBalanced1 = isBracketSequenceCorrect str1 // true
+
+
+printfn "%A" $"{isBalanced1}"
