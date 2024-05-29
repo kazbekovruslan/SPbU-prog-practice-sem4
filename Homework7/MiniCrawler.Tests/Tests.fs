b69666a704b2cebaf35a1ea89ef1077c43be1481
kazbekovruslan <chartusss@yandex.ru>
2024-05-29 20:50:02 +0300

    delete bad test

--- Homework7/MiniCrawler.Tests/Tests.fs
+++ Homework7/MiniCrawler.Tests/Tests.fs
@@ -14,27 +14,3 @@ let ``extractLinks should test`` () =
         let expectedLinks = [ "https://www.iana.org/domains/example" ]
         Assert.AreEqual(expectedLinks, actualLinks)
     }
-
-[<Test>]
-let ``downloadPages test`` () =
-    let url = "https://se.math.spbu.ru"
-
-    async {
-        let! actual = downloadPages url
-        printfn "%A" actual
-
-        let expected =
-            [ ("https://spbu.ru/", 112326)
-              ("https://oops.math.spbu.ru/SE/alumni", 49175)
-              ("https://ru.wikipedia.org/wiki/%D0%A2%D0%B5%D1%80%D0%B5%D1%85%D0%BE%D0%B2,_%D0%90%D0%BD%D0%B4%D1%80%D0%B5%D0%B9_%D0%9D%D0%B8%D0%BA%D0%BE%D0%BB%D0%B0%D0%B5%D0%B2%D0%B8%D1%87",
-               102683)
-              ("https://ru.wikipedia.org/wiki/%D0%A2%D0%B5%D1%80%D0%B5%D1%85%D0%BE%D0%B2,_%D0%90%D0%BD%D0%B4%D1%80%D0%B5%D0%B9_%D0%9D%D0%B8%D0%BA%D0%BE%D0%BB%D0%B0%D0%B5%D0%B2%D0%B8%D1%87",
-               102683)
-              ("https://www.acm.org/binaries/content/assets/education/curricula-recommendations/cc2005-march06final.pdf",
-               758040)
-              ("https://t.me/sysprog_admission", 12477)
-              ("https://oops.math.spbu.ru/SE", 23408)
-              ("https://oops.math.spbu.ru/SE/alumni", 49175) ]
-
-        Assert.AreEqual(expected, actual)
-    }
